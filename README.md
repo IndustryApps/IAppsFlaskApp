f251265188041dfbfdfc7e2e3892295997d657e6
Libin Babu <*Foss2k12>
2021-11-23 14:12:04 +0530

    Eureka client configuration in Flask app

---
+++ README.md
@@ -0,0 +1,30 @@
+# Demo Flask App
+
+## Install dependency libraries
+
+```shell
+pip install -r requirements.txt
+```
+
+## Run app
+
+### Provide app properties
+
+``APPCODE`` and ``HOSTNAME`` should be provided in the sample source code.
+
+**APPCODE** can be found in the App Publish page of the vendor portal.
+
+**HOSTNAME** is your app's domain name.
+
+To run the app.
+```shell
+flask run
+```
+
+or simply
+
+```shell
+python app.py
+```
+
+The app will be running on port ``5000``
--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, send_from_directory, send_file, redirect
+from flask_eureka import Eureka
+
+APPCODE = ''
+HOSTNAME = ''
+
+if not APPCODE:
+    print('AppCode cannot be null or empty. You can find the appCode in your app\'s publish page')
+    exit(0)
+if not HOSTNAME:
+    print('A proper hostname should be provided. You can provide your app\'s public domain name')
+    exit(0)
 
 app = Flask(__name__)
+app.config.update(
+    SERVICE_NAME=APPCODE,
+    EUREKA_SERVICE_URL='https://servicediscovery.uat.industryapps.net/',
+    EUREKA_INSTANCE_HOSTNAME=HOSTNAME,
+    EUREKA_INSTANCE_PORT=80
+)
+
+eureka = Eureka(app)
+eureka.register_service(name=APPCODE)
+
+
+@app.route('/healthcheck')
+def health_check():
+    return redirect("/")
 
 
 @app.route("/<path:path>")
 def static_dir(path):
-    print(path)
     if path is None:
         return send_file("index.html")
     else:
